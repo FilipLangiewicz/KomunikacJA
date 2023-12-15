e2ce9320003fa8459570156539013a7927593504
kaminskaz <01180708@pw.edu.pl>
2023-12-15 19:15:08 +0100

    zmiana znaków cały folder

--- kody/Python/zmiana emoji.py
+++ kody/Python/zmiana emoji.py
@@ -1,3 +1,4 @@
+import os
 import json
 
 def fix_emoji(text):
@@ -5,15 +6,23 @@ def fix_emoji(text):
     final_string = initial_string.encode('ISO-8859-1').decode('UTF-8')
     return final_string
 
-def fix_lines_in_json_file(input_file_path, output_file_path):
-    with open(input_file_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
+def fix_lines_in_json_folder(input_folder_path, output_folder_path):
+    # Create output folder if it doesn't exist
+    os.makedirs(output_folder_path, exist_ok=True)
 
-    # Apply the fix_emoji function to each value in the JSON data
-    fixed_data = recursive_fix(data, fix_emoji)
+    for filename in os.listdir(input_folder_path):
+        if filename.endswith(".json"):
+            input_file_path = os.path.join(input_folder_path, filename)
+            output_file_path = os.path.join(output_folder_path, filename)
 
-    with open(output_file_path, 'w', encoding='utf-8') as file:
-        json.dump(fixed_data, file, ensure_ascii=False, indent=2)
+            with open(input_file_path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+
+            # Apply the fix_emoji function to each value in the JSON data
+            fixed_data = recursive_fix(data, fix_emoji)
+
+            with open(output_file_path, 'w', encoding='utf-8') as file:
+                json.dump(fixed_data, file, ensure_ascii=False, indent=2)
 
 def recursive_fix(data, fix_function):
     if isinstance(data, dict):
@@ -26,6 +35,6 @@ def recursive_fix(data, fix_function):
         return data
 
 # Example usage:
-input_filename = r'C:\\Users\\Zosia\\Desktop\\AAAPROJEKT2\\poufne_dane\\messenger\\brudne\\pshemobalicki_1.json'
-output_filename = r'C:\\Users\\Zosia\\Desktop\\AAAPROJEKT2\\poufne_dane\\messenger\\znaki_zmienione'
-fix_lines_in_json_file(input_filename, output_filename)
+input_folder_path = r'C:\\Users\\Zosia\\Desktop\\AAAPROJEKT2\\poufne_dane\\messenger\\brudne'
+output_folder_path = r'C:\\Users\\Zosia\\Desktop\\AAAPROJEKT2\\poufne_dane\\messenger\\znaki_zmienione'
+fix_lines_in_json_folder(input_folder_path, output_folder_path)
