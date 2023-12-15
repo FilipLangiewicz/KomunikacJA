43abe5136176a34112ea2386b2e9675165e4c3af
FilipLangiewicz <flangiewicz@gmail.com>
2023-12-15 23:22:56 +0100

    heatMap plot

--- kody/R/FL.R
+++ kody/R/FL.R
@@ -1,4 +1,35 @@
 library(dplyr)
 library(ggplot2)
+library(plotly)
+
+main_df <- read.csv("../app/KomunikacJA/appData/heatMap/heatMapData.csv")
+df <- main_df %>% 
+  mutate(date = as.factor(date))
 
 
+
+plot <- df %>%
+  filter(person == "f") %>%
+  filter(app == "mg") %>%
+  group_by(date) %>%
+  summarise(n = n()) %>%
+  ggplot(aes(x = date, y = n)) +
+  geom_point() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+ggplotly(plot)
+
+df <- main_df %>%
+  mutate(date = as.Date(as.character(date), format = "%Y%m%d")) %>%
+  mutate(week = lubridate::floor_date(date, unit = "week"))
+
+plot <- df %>%
+  filter(person == "f", app == "mg") %>%
+  group_by(week) %>%
+  summarise(n = n()) %>%
+  ggplot(aes(x = week, y = n)) +
+  geom_point() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggplotly(plot)
