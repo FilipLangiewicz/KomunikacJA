7d1bbacb6e359a0e0f653eff0dacc22d48430048
kaminskaz <01180708@pw.edu.pl>
2023-12-19 19:28:07 +0100

    zmienione kody żeby dobrze działały z nowa ramka danych

--- kody/R/ZK.R
+++ kody/R/ZK.R
@@ -13,7 +13,7 @@ library(plotly)
 
 #TERAZ zrobimy ramki które możecie wrzucić na repo
 
-data <- read.csv("C:\\Users\\Zosia\\Desktop\\AAAPROJEKT2\\poufne_dane\\messenger\\csvs.csv") #trzeba zrobić dla messengera i insta osobno
+data <- read.csv("C:\\Users\\Zosia\\Desktop\\AAAPROJEKT2\\poufne_dane\\instagram\\csvs.csv") #trzeba zrobić dla messengera i insta osobno
 
 # Filter messages containing emojis
 data_with_emojis <- data %>%
@@ -27,16 +27,16 @@ data_with_emojis$MessageLength <- nchar(data_with_emojis$Content)
 data_with_emojis <- data_with_emojis %>% 
   select(Sender, emojis, Timestamp, GroupOrPriv, MessageLength)
 
-data_with_emojis$Sender[data_with_emojis$Sender != "Zosia Kamińska"] <- "Other" #uważać na imie, ja mam bez ń na insta na przykład
-data_with_emojis$platform <- "fb" #zmienić przy instagramie na "in"
+data_with_emojis$Sender[data_with_emojis$Sender != "Zosia Kaminska"] <- "Other" #uważać na imie, ja mam bez ń na insta na przykład
+data_with_emojis$platform <- "in" #zmienić przy instagramie na "in"
 data_with_emojis$name <- "z"
 
-data_with_emojis$emojis[data_with_emojis$emojis == "character(0)"] <- NA 
+data_with_emojis$emojis[data_with_emojis$emojis == "character(0)"] <- 0 
 
 data_with_emojis$emojis <- sapply(data_with_emojis$emojis, function(vec) paste(vec, collapse = ""))
 View(data_with_emojis)
 
-write.csv(data_with_emojis, "../app/KomunikacJA/appData/emojiData/emoji_fb_z.csv", row.names = FALSE) #zmienić literke i platforme
+write.csv(data_with_emojis, "../app/KomunikacJA/appData/emojiData/emoji_in_z.csv", row.names = FALSE) #zmienić literke i platforme
 #DZIĘKIIIII
 
 
@@ -45,14 +45,17 @@ write.csv(data_with_emojis, "../app/KomunikacJA/appData/emojiData/emoji_fb_z.csv
 
 
 #EMOJI WORDCLOUD
-data <- read.csv("C:\\Users\\Zosia\\Desktop\\AAAPROJEKT2\\poufne_dane\\messenger\\csvs.csv")
+data_mg <- read.csv("../app/KomunikacJA/appData/emojiData/emoji_mg_z.csv")
+data_in <- read.csv("../app/KomunikacJA/appData/emojiData/emoji_in_z.csv")
+
+data <- full_join(data_in, data_mg)
 
 # Filter messages sent by me
 name_data <- data %>%
-  filter(Sender == "Zosia Kamińska")
+  filter(Sender %in% c("Zosia Kamińska", "Zosia Kaminska"))
 
 # Extract emojis from the content
-emoji_list <- str_extract_all(name_data$Content, "[\\x{1F600}-\\x{1F64F}\\x{1F300}-\\x{1F5FF}\\x{1F680}-\\x{1F6FF}\\x{1F700}-\\x{1F77F}\\x{1F780}-\\x{1F7FF}\\x{1F800}-\\x{1F8FF}\\x{1F900}-\\x{1F9FF}\\x{1FA00}-\\x{1FA6F}\\x{2600}-\\x{26FF}\\x{2700}-\\x{27BF}]")
+emoji_list <- str_extract_all(name_data$emojis, "[\\x{1F600}-\\x{1F64F}\\x{1F300}-\\x{1F5FF}\\x{1F680}-\\x{1F6FF}\\x{1F700}-\\x{1F77F}\\x{1F780}-\\x{1F7FF}\\x{1F800}-\\x{1F8FF}\\x{1F900}-\\x{1F9FF}\\x{1FA00}-\\x{1FA6F}\\x{2600}-\\x{26FF}\\x{2700}-\\x{27BF}]")
 all_emojis <- unlist(emoji_list)
 
 # Create a data frame with emoji frequencies
@@ -77,7 +80,7 @@ wordcloud2(
   minRotation = 0,
   maxRotation = 0,
   rotateRatio = 0,
-  gridSize = 15,
+  gridSize = 5,
   shape = "circle",
   shuffle = FALSE
 )
@@ -88,21 +91,19 @@ wordcloud2(
 zosia_data <- data %>%
   filter(Sender == "Zosia Kamińska")
 
-# Calculate the number of characters in each message
-zosia_data$MessageLength <- nchar(zosia_data$Content)
-
 # Calculate the average message length
 average_length <- mean(zosia_data$MessageLength)
+
 
 # Print the result
 cat("Average message length sent by Zosia Kamińska:", round(average_length, 2), "characters\n")
 
 # Find the shortest and longest messages
-shortest_message <- zosia_data[which.min(zosia_data$MessageLength), c("Content", "MessageLength")]
-longest_message <- zosia_data[which.max(zosia_data$MessageLength), c("Content", "MessageLength")]
+shortest_message <- zosia_data[which.min(zosia_data$MessageLength), c("MessageLength")]
+longest_message <- zosia_data[which.max(zosia_data$MessageLength), c("MessageLength")]
 
-cat("Shortest message (", shortest_message$MessageLength, " characters)", "\n")
-cat("Longest message (", longest_message$MessageLength, " characters)", "\n")
+cat("Shortest message (", shortest_message, " characters)", "\n")
+cat("Longest message (", longest_message, " characters)", "\n")
 
 
 #BOXPLOT Z DŁUGOŚCIAMI WIADOMOŚCI
@@ -128,11 +129,12 @@ boxplot
 #AVERAGE LICZBA EMOTEK NA WIADOMOŚĆ
 
 # Extract emojis from the content
-emoji_list <- str_extract_all(zosia_data$Content, "[\\x{1F600}-\\x{1F64F}\\x{1F300}-\\x{1F5FF}\\x{1F680}-\\x{1F6FF}\\x{1F700}-\\x{1F77F}\\x{1F780}-\\x{1F7FF}\\x{1F800}-\\x{1F8FF}\\x{1F900}-\\x{1F9FF}\\x{1FA00}-\\x{1FA6F}\\x{2600}-\\x{26FF}\\x{2700}-\\x{27BF}]")
+emoji_list <- str_extract_all(zosia_data$emojis, "[\\x{1F600}-\\x{1F64F}\\x{1F300}-\\x{1F5FF}\\x{1F680}-\\x{1F6FF}\\x{1F700}-\\x{1F77F}\\x{1F780}-\\x{1F7FF}\\x{1F800}-\\x{1F8FF}\\x{1F900}-\\x{1F9FF}\\x{1FA00}-\\x{1FA6F}\\x{2600}-\\x{26FF}\\x{2700}-\\x{27BF}]")
 
 # Count the number of emojis in each message
 zosia_data$NumEmojis <- lengths(emoji_list)
-
+zosia_data$NumEmojis[is.na(zosia_data$emojis)] <- 0
+View(zosia_data)
 # Calculate the average number of emojis per message
 average_emojis_per_message <- mean(zosia_data$NumEmojis)
 
@@ -150,29 +152,33 @@ cat("Average number of emojis per message sent by Zosia Kamińska:", round(avera
 
 #ANIMATED BAR PLOT USING PLOTLY
 
-data <- read.csv("C:\\Users\\Zosia\\Desktop\\AAAPROJEKT2\\poufne_dane\\messenger\\csvs.csv")
+convert_to_vector <- function(emoji_string) {
+  if (!is.na(emoji_string)) {
+    return(unlist(strsplit(emoji_string, "")))
+  } else {
+    return(NA)
+  }
+}
+
 
 # Filter messages containing emojis
-data_with_emojis <- data %>%
-  filter(str_detect(Content, "[\\x{1F600}-\\x{1F64F}\\x{1F300}-\\x{1F5FF}\\x{1F680}-\\x{1F6FF}\\x{1F700}-\\x{1F77F}\\x{1F780}-\\x{1F7FF}\\x{1F800}-\\x{1F8FF}\\x{1F900}-\\x{1F9FF}\\x{1FA00}-\\x{1FA6F}\\x{2600}-\\x{26FF}\\x{2700}-\\x{27BF}]")) %>%
-  mutate(emojis = str_extract_all(Content, "[\\x{1F600}-\\x{1F64F}\\x{1F300}-\\x{1F5FF}\\x{1F680}-\\x{1F6FF}\\x{1F700}-\\x{1F77F}\\x{1F780}-\\x{1F7FF}\\x{1F800}-\\x{1F8FF}\\x{1F900}-\\x{1F9FF}\\x{1FA00}-\\x{1FA6F}\\x{2600}-\\x{26FF}\\x{2700}-\\x{27BF}]"))
+data_with_emojis <- data
 
-# Extract timestamps with milliseconds
-data_with_emojis$Timestamp <- as.POSIXct(data_with_emojis$Timestamp / 1000, origin = "1970-01-01", tz = "GMT")
+data_with_emojis <- data_with_emojis %>% 
+mutate(emojis = sapply(emojis, convert_to_vector))
 
-View(data_with_emojis)
-# Pivot the data
 pivoted_data <- data_with_emojis %>%
-  select(Sender, Timestamp, emojis) %>%
+  filter(Sender == "Zosia Kamińska") %>% 
+  select(Timestamp, emojis, name, platform) %>%
   unnest(emojis) %>%
-  group_by(Sender, Timestamp, emojis) %>% 
+  group_by(Timestamp, emojis, name, platform) %>% 
   summarise(count = n()) %>%
-  arrange(emojis, Timestamp) %>%
-  group_by(Sender, emojis) %>%
-  mutate(cumulative_count = cumsum(count)) %>% 
-  filter(Sender == "Zosia Kamińska")
+  arrange(emojis, Timestamp, name, platform) %>%
+  group_by(emojis) %>%
+  mutate(cumulative_count = cumsum(count)) 
 
-View(pivoted_data)
+pivoted_data <- pivoted_data %>% filter(platform == "fb")
+pivoted_data <-  pivoted_data %>% filter(!is.na(emojis))
 
 # Select the top 10 emojis based on cumulative_count
 top_10 <- pivoted_data %>%
@@ -191,15 +197,15 @@ pivoted_data <- pivoted_data %>%
 selected_data <- pivoted_data %>%
   filter(emojis %in% top_10)
 
-selected_data <- selected_data %>% select(emojis, month_year, count)
+selected_data <- selected_data %>% select(emojis, month_year, count, name)
 
 # Create a combination of all emojis and months for each sender
 all_combinations <- expand_grid(emojis = unique(selected_data$emojis),
                                 month_year = unique(selected_data$month_year),
-                                Sender = unique(selected_data$Sender))
+                                name = unique(selected_data$name))
 
 # Merge with selected_data to fill missing combinations with count 0
-complete_data <- left_join(all_combinations, selected_data, by = c("emojis", "month_year", "Sender")) %>%
+complete_data <- left_join(all_combinations, selected_data, by = c("emojis", "month_year", "name")) %>%
   replace_na(list(count = 0))
 
 # Calculate cumulative count for each month
