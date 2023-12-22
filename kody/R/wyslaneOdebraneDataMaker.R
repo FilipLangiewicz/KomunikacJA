f2a5268bad73c580b59264f560ee56d894e6477c
Anna Ostrowska <01180729@pw.edu.pl>
2023-12-22 21:06:36 +0100

    wyslane/odebrane Ania + kod

---
+++ kody/R/wyslaneOdebraneDataMaker.R
@@ -0,0 +1,61 @@
+library(dplyr)
+
+#sciezki do brudnych csv (tych samych, co wrzucaliscie do Zosi przy emoji)
+#ZMIENIC LITERKI
+ig_a <- read.csv("C:\\twd_proj2\\poufne_dane\\instagram\\csv.csv")
+sp_a <- read.csv("C:\\twd_proj2\\repo\\Projekt_TWD_02\\kody\\data_csv\\sp_a.csv")
+mg_a <- read.csv("C:\\twd_proj2\\poufne_dane\\messenger\\csv.csv")
+
+konwertujTimestampy <- function(df) {
+  
+  
+  # Konwersja kolumny timestamp na daty
+  df$rawDate <- as.POSIXct(df$Timestamp / 1000, origin = "1970-01-01", tz = "Europe/Warsaw")
+  
+  # Formatowanie dat w formacie YYYYMMDD
+  df$date <- format(df$rawDate, "%Y%m%d")
+  df$strDate <- format(df$rawDate, "%d-%m-%Y")
+  df$year <- format(df$rawDate, "%Y")
+  df$month <- format(df$rawDate, "%m")
+  df$day <- format(df$rawDate, "%d")
+  return(df)
+}
+
+wybierzOdpowiednieKolumny <- function(df) {
+  return(df %>% 
+           select(Sender, GroupOrPriv, date)
+  )
+}
+
+konwertujIWybierz <- function(df){
+  df <- konwertujTimestampy(df)
+  df <- wybierzOdpowiednieKolumny(df)
+  return(df)
+}
+
+
+zmienDaneSnap <- function(df){
+  return(
+    df %>% 
+      rename(Timestamp = Created.microseconds.) %>% 
+      rename(Sender=IsSender)
+  )
+}
+#WAZNE - pozmieniac literki!
+sp_a <- zmienDaneSnap(sp_a)
+mg_a <- konwertujIWybierz(mg_a)
+ig_a <- konwertujIWybierz(ig_a)
+sp_a <- konwertujTimestampy(sp_a)
+sp_a <- sp_a %>% 
+  select(Sender,date)
+
+#ZMIENIC LITERKI I NAZWISKA
+mg_a$Sender[mg_a$Sender != "Anna Ostrowska"] <- "Other"
+ig_a$Sender[ig_a$Sender != "Anna Ostrowska"] <- "Other"
+sp_a$Sender[sp_a$Sender != "TRUE"] <- "Other"
+sp_a$Sender[sp_a$Sender == "TRUE"] <- "Anna Ostrowska"
+
+#TU TEZ ZMIENIC
+write.csv(mg_a, "C:/twd_proj2/repo/Projekt_TWD_02/app/KomunikacJA/appData/WyslaneOdebrane/WyslaneOdebraneData/mg_a.csv", row.names = FALSE)
+write.csv(ig_a, "C:/twd_proj2/repo/Projekt_TWD_02/app/KomunikacJA/appData/WyslaneOdebrane/WyslaneOdebraneData/ig_a.csv", row.names = FALSE)
+write.csv(sp_a, "C:/twd_proj2/repo/Projekt_TWD_02/app/KomunikacJA/appData/WyslaneOdebrane/WyslaneOdebraneData/sp_a.csv", row.names = FALSE)
