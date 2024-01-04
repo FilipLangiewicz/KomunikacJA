8b604915186ac3b713f0bc5817cb2f088a28ac87
Anna Ostrowska <01180729@pw.edu.pl>
2024-01-04 17:12:16 +0100

    wyslaneOdebrane dane Zosi

--- kody/R/wyslaneOdebraneDataMaker.R
+++ kody/R/wyslaneOdebraneDataMaker.R
@@ -1,13 +1,13 @@
 library(dplyr)
-
+library(lubridate)
 #sciezki do brudnych csv z mg i ig (tych samych, co wrzucaliscie do Zosi przy emoji)
 #ZMIENIC LITERKI
 #ze snapa ja wzielam juz Filipa, Zosi jeszcze nie ma:()
-ig_a <- read.csv("C:\\twd_proj2\\poufne_dane\\instagram\\csv.csv")
+ig_z <- read.csv("C:\\twd_proj2\\repo\\Projekt_TWD_02\\app\\KomunikacJA\\appData\\emojiData\\emoji_in_z.csv")
 sp_a <- read.csv("C:\\twd_proj2\\repo\\Projekt_TWD_02\\kody\\data_csv\\sp_a.csv")
-mg_a <- read.csv("C:\\twd_proj2\\poufne_dane\\messenger\\csv.csv")
+ig_z <- read.csv("C:\\twd_proj2\\repo\\Projekt_TWD_02\\app\\KomunikacJA\\appData\\emojiData\\emoji_in_z.csv")
 
-sp_f <- read.csv("C:\\twd_proj2\\repo\\Projekt_TWD_02\\kody\\data_csv\\sp_f.csv")
+sp_z <- read.csv("C:\\twd_proj2\\repo\\Projekt_TWD_02\\kody\\data_csv\\sp_z.csv")
 konwertujTimestampy <- function(df) {
   
   
@@ -23,6 +23,7 @@ konwertujTimestampy <- function(df) {
   return(df)
 }
 
+
 wybierzOdpowiednieKolumny <- function(df) {
   return(df %>% 
            select(Sender, GroupOrPriv, date)
@@ -31,6 +32,12 @@ wybierzOdpowiednieKolumny <- function(df) {
 
 konwertujIWybierz <- function(df){
   df <- konwertujTimestampy(df)
+  df <- wybierzOdpowiednieKolumny(df)
+  return(df)
+}
+
+konwertujIWybierz <- function(df){
+  df <- konwertujTimestampy_mg_z(df)
   df <- wybierzOdpowiednieKolumny(df)
   return(df)
 }
@@ -44,20 +51,25 @@ zmienDaneSnap <- function(df){
   )
 }
 #WAZNE - pozmieniac literki!
-sp_f <- zmienDaneSnap(sp_f)
-mg_a <- konwertujIWybierz(mg_a)
+sp_z <- zmienDaneSnap(sp_z)
+mg_z <- konwertujTimestampy_mg_z(mg_z)
 ig_a <- konwertujIWybierz(ig_a)
-sp_f <- konwertujTimestampy(sp_f)
-sp_f <- sp_f %>% 
+sp_z<- konwertujTimestampy(sp_z)
+sp_z <- sp_z %>% 
   select(Sender,date)
+date<- ymd_hms(ig_z$Timestamp)
 
+# Wydobywanie samej daty w formie liczby całkowitej
+ig_z$date <- as.numeric(format(date, "%Y%m%d"))
+ig_z %>% 
+  select(Sender, GroupOrPriv, date) ->ig_z
 #ZMIENIC LITERKI I NAZWISKA
-mg_a$Sender[mg_a$Sender != "Anna Ostrowska"] <- "Other"
+mg_a$Sender[mg_a$Sender != "Zosia Kamińska"] <- "Other"
 ig_a$Sender[ig_a$Sender != "Anna Ostrowska"] <- "Other"
-sp_f$Sender[sp_a$Sender != "TRUE"] <- "Other"
-sp_f$Sender[sp_a$Sender == "TRUE"] <- "Anna Ostrowska"
+sp_z$Sender[sp_z$Sender != "TRUE"] <- "Other"
+sp_z$Sender[sp_z$Sender == "TRUE"] <- "Zosia Kaminska"
 
 #TU TEZ ZMIENIC
-write.csv(mg_a, "../app/KomunikacJA/appData/wyslaneOdebrane/wyslaneOdebrane_mg_f.csv", row.names = FALSE)
-write.csv(ig_a, "../app/KomunikacJA/appData/wyslaneOdebrane/wyslaneOdebrane_ig_f.csv", row.names = FALSE)
-write.csv(sp_f, "../app/KomunikacJA/appData/wyslaneOdebrane/wyslaneOdebrane_sp_f.csv", row.names = FALSE)
+write.csv(mg_z, "../app/KomunikacJA/appData/wyslaneOdebrane/wyslaneOdebrane_mg_z.csv", row.names = FALSE)
+write.csv(ig_z, "../app/KomunikacJA/appData/wyslaneOdebrane/wyslaneOdebrane_ig_z.csv", row.names = FALSE)
+write.csv(sp_z, "../app/KomunikacJA/appData/wyslaneOdebrane/wyslaneOdebrane_sp_z.csv", row.names = FALSE)
